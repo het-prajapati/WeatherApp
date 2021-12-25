0b76942ea7cc433d08bbe590dfc4327997cb3c68
Het Prajapati <hprajapati@albany.edu>
2021-12-25 16:12:27 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Het Prajapati on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
